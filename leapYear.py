4e63bc507878fbd55cfccf4fc7574b8d684d297e
zacharyenright <70986414+zacharyenright@users.noreply.github.com>
2022-09-19 10:04:45 -0400

    Update leapYear.py

--- leapYear.py
+++ leapYear.py
@@ -2,14 +2,16 @@ is_leap_year = False
    
 input_year = int(input())
 
-if ((input_year // 100) % 4) != 0:
-    is_leap_year = True
-    print(input_year, "- not a leap year")
-    exit()
-if (input_year % 4) == 0:
+
+
+if (input_year % 400) == 0:
     is_leap_year = True
     print(input_year, "- leap year")
-elif (input_year % 400) == 0:
+    exit()
+elif (input_year % 100) == 0:
+    is_leap_year = False
+    print(input_year, "- not a leap year")
+elif (input_year % 4) == 0:
     is_leap_year = True
     print(input_year, "- leap year")
 
